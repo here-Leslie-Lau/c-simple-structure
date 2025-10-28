257e7189e0ad5bd9a7e2baff6de2b1ec1c9c50c3
Leslie-Lau <804738464@qq.com>
2025-10-28 23:52:14 +0800

    feat(node): add linked list test and print function

--- node/main.c
+++ node/main.c
@@ -1,13 +1,36 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "node.h"
 
+void print_node(node_t *head);
+
 int main(void) {
-  node_t *head = create_node(10);
+  node_t *head = create_node(0);
   if (head == NULL) {
     return 1;
   }
 
-  // TODO leslie: add your tests here
+  for (int i = 1; i < 10; i++) {
+    node_t *node = create_node(i);
+    if (push_node_in_tail(head, node) == false) {
+      free(node);
+      continue;
+    }
+  }
+
+  print_node(head);
+
+  free_node(head);
   return 0;
+}
+
+void print_node(node_t *head) {
+  node_t *curr = head;
+  while (curr != NULL) {
+    printf("%d ", curr->data);
+    curr = curr->next;
+  }
+
+  printf("\n");
 }
--- node/node.c
+++ node/node.c
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <stdlib.h>
 
 #include "node.h"
